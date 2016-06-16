a0ec9f917c0096a42bb00471726f8880db6fb2e1
Roman Dvornov <rdvornov@gmail.com>
2016-06-17 01:44:17 +0300

    no more Scanner#columns

--- test/parse.js
+++ test/parse.js
@@ -1,3 +1,4 @@
+var BOM = String.fromCharCode(0xFEFF);
 var path = require('path');
 var assert = require('assert');
 var csso = require('../lib/index');
@@ -21,6 +22,23 @@ function createParseErrorTest(location, test, options) {
         assert.equal(error.message, test.error);
         assert.deepEqual(error.parseError, test.position);
     });
+}
+
+function createPositionsTest(css, expected, initLine, initColumn) {
+    return function() {
+        var positions = [];
+        var ast = csso.parse(css, {
+            positions: true,
+            line: initLine,
+            column: initColumn
+        });
+
+        csso.walk(ast, function(node) {
+            positions.unshift([node.info.line, node.info.column, node.type]);
+        });
+
+        assert.deepEqual(positions, expected);
+    };
 }
 
 describe('parse', function() {
@@ -59,17 +77,10 @@ describe('parse error', function() {
 });
 
 describe('positions', function() {
-    it('should start with line 1 column 1 by default', function() {
-        var ast = csso.parse('.foo.bar {\n  property: value;\n}', {
-            positions: true
-        });
-        var positions = [];
+    var css = '.foo.bar {\n  property: value;\n}';
 
-        csso.walk(ast, function(node) {
-            positions.unshift([node.info.line, node.info.column, node.type]);
-        });
-
-        assert.deepEqual(positions, [
+    describe('should start with line 1 column 1 by default', function() {
+        var positions = [
             [1, 1, 'StyleSheet'],
             [1, 1, 'Ruleset'],
             [1, 10, 'Block'],
@@ -81,22 +92,33 @@ describe('positions', function() {
             [1, 1, 'SimpleSelector'],
             [1, 5, 'Class'],
             [1, 1, 'Class']
-        ]);
+        ];
+
+        it('regular', createPositionsTest(css, positions));
+        it('with BOM', createPositionsTest(BOM + css, positions));
+    });
+
+    it('should start with line 1 and specified column', function() {
+        var positions = [
+            [1, 5, 'StyleSheet'],
+            [1, 5, 'Ruleset'],
+            [1, 14, 'Block'],
+            [2, 3, 'Declaration'],
+            [2, 12, 'Value'],
+            [2, 13, 'Identifier'],
+            [2, 3, 'Property'],
+            [1, 5, 'Selector'],
+            [1, 5, 'SimpleSelector'],
+            [1, 9, 'Class'],
+            [1, 5, 'Class']
+        ];
+
+        it('regular', createPositionsTest(css, positions, undefined, 5));
+        it('with BOM', createPositionsTest(BOM + css, positions, undefined, 5));
     });
 
     it('should start with specified line and column', function() {
-        var ast = csso.parse('.foo.bar {\n  property: value;\n}', {
-            positions: true,
-            line: 3,
-            column: 5
-        });
-        var positions = [];
-
-        csso.walk(ast, function(node) {
-            positions.unshift([node.info.line, node.info.column, node.type]);
-        });
-
-        assert.deepEqual(positions, [
+        var positions = [
             [3, 5, 'StyleSheet'],
             [3, 5, 'Ruleset'],
             [3, 14, 'Block'],
@@ -108,6 +130,28 @@ describe('positions', function() {
             [3, 5, 'SimpleSelector'],
             [3, 9, 'Class'],
             [3, 5, 'Class']
-        ]);
+        ];
+
+        it('regular', createPositionsTest(css, positions, 3, 5));
+        it('with BOM', createPositionsTest(BOM + css, positions, 3, 5));
+    });
+
+    it('should start with specified line and column 1', function() {
+        var positions = [
+            [3, 1, 'StyleSheet'],
+            [3, 1, 'Ruleset'],
+            [3, 10, 'Block'],
+            [4, 3, 'Declaration'],
+            [4, 12, 'Value'],
+            [4, 13, 'Identifier'],
+            [4, 3, 'Property'],
+            [3, 1, 'Selector'],
+            [3, 1, 'SimpleSelector'],
+            [3, 5, 'Class'],
+            [3, 1, 'Class']
+        ];
+
+        it('regular', createPositionsTest(css, positions, 3));
+        it('with BOM', createPositionsTest(BOM + css, positions, 3));
     });
 });
